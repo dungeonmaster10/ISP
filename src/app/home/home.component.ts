e440b625a9fd244e0b9d13148b277e68331259d9
rushikesh <rushikesh.karnewar@capco.com>
2019-01-21 20:00:07 +0530

    table added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  data: Array<any> = [
+    { area: 'abc', connections: 45678, money: 123456 },
+    { area: 'pqr', connections: 45678, money: 123456 },
+    { area: 'xyz', connections: 45678, money: 123456 },
+    { area: 'def', connections: 45678, money: 123456 },
+    { area: 'qwe', connections: 45678, money: 123456 },
+    { area: 'ytr', connections: 45678, money: 123456 },
+  ];
   constructor() { }
 
   ngOnInit() {
